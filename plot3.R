8a55b95066cb40d9d39978c76cda6df2e5444428
tanya kanodia <tanya.kanodia@gmail.com>
2021-01-21 18:47:19 +0530

    commit

--- plot3.R
+++ plot3.R
@@ -13,4 +13,3 @@ lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend(c("topright"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
 dev.off()
-
